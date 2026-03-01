ff4c5c2df7b48b4d64af218dda547d29dc89a956
LarissaA95 <gero.larissaandrade@gmail.com>
2026-03-01 19:08:56 -0300

    Moldels atualizado

--- AppLembreteMedicacao/AppLembreteMedicacao/Helpers/SQLiteDatabaseHelper.cs
+++ AppLembreteMedicacao/AppLembreteMedicacao/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using AppLembreteMedicacao.Models;
 
 namespace AppLembreteMedicacao.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Medicamento>().Wait();
+            _conn.CreateTableAsync<Horario>().Wait();
+            _conn.CreateTableAsync<HistoricoUso>().Wait();
+            _conn.CreateTableAsync<Usuario>().Wait();
+        }
+// MEDICAMENTO
+
+        public Task<int> InsertMedicamento(Medicamento m)
+            => _conn.InsertAsync(m);
+
+        public Task<List<Medicamento>> GetMedicamentos()
+            => _conn.Table<Medicamento>().ToListAsync();
+
+        public Task<int> UpdateMedicamento(Medicamento m)
+            => _conn.UpdateAsync(m);
+
+        public Task<int> DeleteMedicamento(int id)
+            => _conn.DeleteAsync<Medicamento>(id);
+
+        // HORARIO
+
+        public Task<int> InsertHorario(Horario h)
+            => _conn.InsertAsync(h);
+
+        public Task<List<Horario>> GetHorarios(int medicamentoId)
+            => _conn.Table<Horario>()
+                    .Where(h => h.MedicamentoId == medicamentoId)
+                    .ToListAsync();
+
+        public Task<int> DeleteHorario(int id)
+            => _conn.DeleteAsync<Horario>(id);
+
+        // HISTÓRICO 
+
+        public Task<int> InsertHistorico(HistoricoUso h)
+            => _conn.InsertAsync(h);
+
+        public Task<List<HistoricoUso>> GetHistorico(int medicamentoId)
+            => _conn.Table<HistoricoUso>()
+                    .Where(h => h.MedicamentoId == medicamentoId)
+                    .ToListAsync();
+
+        public Task<int> AtualizarStatusHistorico(HistoricoUso h)
+            => _conn.UpdateAsync(h);
+
+        // USUARIO
+
+        public Task<int> InsertUsuario(Usuario u)
+            => _conn.InsertAsync(u);
+
+        public Task<Usuario> GetUsuarioByEmail(string email)
+            => _conn.Table<Usuario>()
+                    .Where(u => u.Email == email)
+                    .FirstOrDefaultAsync();
+
+        public Task<int> UpdateUsuario(Usuario u)
+            => _conn.UpdateAsync(u);
+
+        public Task<int> DeleteUsuario(int id)
+            => _conn.DeleteAsync<Usuario>(id);
     }
 }
--- AppLembreteMedicacao/AppLembreteMedicacao/Models/Usuario.cs
+++ AppLembreteMedicacao/AppLembreteMedicacao/Models/Usuario.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace AppLembreteMedicacao.Models
 {
-    internal class Usuario
+    public class Usuario
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nome { get; set; }
+
+        [NotNull, Unique]
+        public string Email { get; set; }
+
+        [NotNull]
+        public string SenhaHash { get; set; }
+
+        public int Ativo { get; set; } = 1;
     }
 }
